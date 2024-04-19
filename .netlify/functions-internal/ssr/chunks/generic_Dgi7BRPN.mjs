8b3e9d02d1b134c96f5b8227acc9a9a7a7fdea22
Jonathan <jonathan0gab24@gmail.com>
2024-04-19 18:36:07 -0300

    aaa

---
+++ .netlify/functions-internal/ssr/chunks/generic_Dgi7BRPN.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_CS-CMi0N.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/ssr/chunks/index_DI9HF52Q.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_s5yBtjCH.mjs');
+
+export { page };
